a13ab4faa84a818385d70036893c773dd3090cb8
Ariasbx9 <yabrielaariasperez@gmail.com>
2024-10-22 18:42:51 -0600

    actualizacion crud usuario

--- ProyectoBlockNotas/src/main/java/com/mycompany/proyectoblocknotas/Usuario/ControladorUsuario.java
+++ ProyectoBlockNotas/src/main/java/com/mycompany/proyectoblocknotas/Usuario/ControladorUsuario.java
@@ -4,10 +4,50 @@
  */
 package com.mycompany.proyectoblocknotas.Usuario;
 
+import com.mycompany.proyectoblocknotas.Nota.Nota;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Usuario
  */
 public class ControladorUsuario {
     
+    
+    private static final String FILE_NAME ="usuario.dat";
+    private List<Usuario> usuarios;
+    
+    
+    private void cargarDatos() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            usuarios = (List<Usuario>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("Archivo no encontrado, se creará uno nuevo.");
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            
+            
+        }
+         }
+    
+     public void NotasController() {
+        usuarios = new ArrayList<>();
+        cargarDatos();
+    }
+     
+      // Crear
+    public void agregarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+        guardarDatos();
+        
+    }
+
+    private void guardarDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }

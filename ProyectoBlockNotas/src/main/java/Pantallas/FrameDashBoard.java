1c536dc8037c9729341b424890ea82f0a039f801
Chermi3541 <jeremyvalverde2011@gmail.com>
2024-11-04 20:08:17 -0600

    crear la pantalla de buscar

--- ProyectoBlockNotas/src/main/java/Pantallas/FrameDashBoard.java
+++ ProyectoBlockNotas/src/main/java/Pantallas/FrameDashBoard.java
@@ -66,7 +66,7 @@ public class FrameDashBoard extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void CBMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CBMenuActionPerformed
-     JpHistorico panelHistorico;
+     JpHistorico panelHistorico =  new JpHistorico();
      panelHistorico.setVisible(true);
 // TODO add your handling code here:
     }//GEN-LAST:event_CBMenuActionPerformed
